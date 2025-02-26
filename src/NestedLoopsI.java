2db8e7581c9128a60a7b725395b3cd46b7c7f782
djstackzzzz <sissokdi@mail.uc.edu>
2025-02-26 18:28:08 -0500

    Initial commit

--- src/NestedLoopsI.java
+++ src/NestedLoopsI.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+public class NestedLoopsI extends NestedLoops {
+}
+class NestedLoops {
+    public static void main(String[] args) {
+        // Task 5: Right-angled triangle
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
-public class NestedLoopsI {
+        // Task 6: Inverted right-angled triangle
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Task 7: Square of asterisks
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
